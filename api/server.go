a068fb5a32a661717c55b7ce4971d6260a8987e5
Julian Sunn <juliansunn@gmail.com>
2023-11-05 06:28:25 -0800

    api and env implementation

---
+++ api/server.go
@@ -0,0 +1,49 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/juliansunn/simple_bank/db/sqlc"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new Server instance with the given store.
+//
+// Parameters:
+// - store: a pointer to a db.Store object.
+//
+// Returns:
+// - a pointer to the newly created Server object.
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccounts)
+
+	server.router = router
+	return server
+}
+
+// Start starts the server on the specified address.
+//
+// Parameters:
+// - address: the address to listen on.
+//
+// Returns:
+// - error: an error if the server failed to start.
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+// errorResponse generates an error response in the form of a gin.H map.
+//
+// The function takes an error as a parameter and returns a gin.H map
+// with a single key "error" and the error message as its value.
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
---
+++ util/config.go
@@ -0,0 +1,29 @@
+package util
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+// LoadConfig loads the configuration from the specified path.
+//
+// path: the path to the configuration file.
+// config: the loaded configuration.
+// err: any error that occurred during the loading process.
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+	err = viper.Unmarshal(&config)
+	return
+}
