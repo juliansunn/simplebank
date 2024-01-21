b3d6740eb551541e3fba5714fcfd50dd2a33d809
Julian Sunn <44549978+juliansunn@users.noreply.github.com>
2024-01-21 14:20:34 -0800

    added test send email and configure func to send real emails (#27)
    * added test send email and configure func to send real emails
    
    * skip add test email test

---
+++ mail/sender_test.go
@@ -0,0 +1,30 @@
+package mail
+
+import (
+	"testing"
+
+	"github.com/juliansunn/simple_bank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSendEmailWithGmail(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	config, err := util.LoadConfig("..")
+	require.NoError(t, err)
+
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+
+	subject := "A test email"
+	content := `
+		<h1>Hello, world!</h1>
+		<p>This is a test email.</p>
+	`
+	to := []string{"juliansunn@gmail.com"}
+	attachFiles := []string{"../README.md"}
+
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
+
+}
